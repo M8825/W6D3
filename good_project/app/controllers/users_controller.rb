17a0ff5bd8be99205444ee0d4e11ecb96485ae5d
Mlkz <maho.mamulashvili@gmail.com>
2022-12-14 16:33:41 -0500

    Add users controller

---
+++ good_project/app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+    render plain: "I'm in the index action!"
+  end
+
+  def create
+    render json: params
+  end
+
+  def show
+    render plain: params
+  end
+end
